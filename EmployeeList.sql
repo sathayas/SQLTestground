7dd53b34249c6eaefe96523a00fc5286ff2b4c79
Satoru Hayasaka <hayasaka@utexas.edu>
2019-09-05 15:57:21 -0500

    Now selected office numbers are reported

--- EmployeeList.sql
+++ EmployeeList.sql
@@ -18,10 +18,28 @@ FROM employees
 ORDER BY lastName ASC, firstName ASC;
 
 --Sorted by title in a specified order, then by names
-SELECT lastName, firstName, jobTitle
+SELECT lastName, firstName, jobTitle, officeCode
 FROM employees
 ORDER BY
   FIELD(jobTitle, 'President', 'VP Marketing',
         'VP Sales', 'Sales Manager (APAC)',
         'Sale Manager (EMEA)', 'Sales Manager (NA)', 'Sales Rep'),
   lastName ASC, firstName ASC;
+
+--Only the sales reps
+SELECT lastName, firstName, jobTitle, officeCode
+FROM employees
+WHERE jobTitle = 'Sales Rep'
+ORDER BY officeCode;
+
+--Only the sales reps from office 6
+SELECT lastName, firstName, jobTitle, officeCode
+FROM employees
+WHERE jobTitle = 'Sales Rep' AND officeCode=6
+ORDER BY officeCode;
+
+--Only the sales reps from offices 1, 2, and 6
+SELECT lastName, firstName, jobTitle, officeCode
+FROM employees
+WHERE jobTitle = 'Sales Rep' AND officeCode in (1,2,6)
+ORDER BY officeCode;
